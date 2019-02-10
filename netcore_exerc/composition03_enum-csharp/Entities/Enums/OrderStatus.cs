98c77cbe10c6021d1087a9c8c61c14ca11f5ead5
Marcio Venancio <marcio.venancio@gmail.com>
2019-02-10 14:51:53 -0200

    composition 3

---
+++ netcore_exerc/composition03_enum-csharp/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace composition03_enum_csharp.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        Pending_payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    };
+}
---
+++ netcore_exerc/composition03_enum-csharp/Entities/Product.cs
@@ -0,0 +1,13 @@
+namespace composition03_enum_csharp.Entities {
+    class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product () {
+
+        }
+        public Product (string name, double price) {
+            Name = name;
+            Price = price;
+        }
+    }
+}
